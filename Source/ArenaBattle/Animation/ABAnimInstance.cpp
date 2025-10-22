644e80def4c4d35a50a1746a3869c8e191cdc03f
soloplay <somnium1108@gmail.com>
2025-10-22 10:12:35 +0900

    이벤트 그래프에서 사용될 변수 선언 및 간단한 세팅

---
+++ Source/ArenaBattle/Animation/ABAnimInstance.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/ABAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UABAnimInstance::UABAnimInstance()
+{
+	// 이동을 판단할 때 사용할 기준 값 설정.
+	MovingThreshold = 3.0f;
+}
+
+void UABAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 계속 사용할 참조 값 설정.
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (Owner != nullptr)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		// 캐릭터 무브먼트 컴포넌트의 현재 속도(방향 빠르기)
+		Velocity = Movement->Velocity;
+
+		// 지면에서의 이동 빠르기 구하기.
+		GroundSpeed = Velocity.Size2D();
+
+		// 이동 / 정지 상태 설정
+		bIsIdle = GroundSpeed < MovingThreshold;
+	}
+}
